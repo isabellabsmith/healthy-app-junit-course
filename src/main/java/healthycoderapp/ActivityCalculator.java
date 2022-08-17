c19fce8dbc16a47566264de54b8214c85db5d295
isabellabsmith <isabellabs@ciandt.com>
2022-08-17 14:58:43 -0300

    Added implementation of the new class according to TDD practice

--- src/main/java/healthycoderapp/ActivityCalculator.java
+++ src/main/java/healthycoderapp/ActivityCalculator.java
@@ -1,8 +1,22 @@
 package healthycoderapp;
 
 public class ActivityCalculator {
+    private static final int WORKOUT_DURATION_MIN = 45;
+
     public static String rateAcitivityLevel(
             int weeklyCardioMin, int weeklyWorkoutSessions) {
-        return "";
+        int totalMinutes = weeklyCardioMin + weeklyWorkoutSessions * WORKOUT_DURATION_MIN;
+        double avgDailyActivityMins = totalMinutes / 7.0;
+
+        if(weeklyCardioMin < 0 || weeklyWorkoutSessions < 0) {
+            throw new RuntimeException("Input bellow 0");
+        }
+        if(avgDailyActivityMins < 20) {
+            return "bad";
+        } else if ( avgDailyActivityMins < 40) {
+            return "average";
+        } else {
+            return "good";
+        }
     }
 }
